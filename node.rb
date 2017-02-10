693ad353d16fc64a80673a02c4d6628ffdd0ba73
Dumitru Gutu <dumitrugutu88@gmail.com>
2017-02-10 11:30:55 -0600

    Finish Node.rb

--- node.rb
+++ node.rb
@@ -1,3 +1,16 @@
 class Node
+  attr_reader :element, :next
 
+  def initialize(value)
+    @element = value
+    @next = nil
+  end
+
+  def insert_after(node)
+    @next = node
+  end
+
+  def remove_after
+    @next = nil
+  end
 end
--- node_spec.rb
+++ node_spec.rb
@@ -1,5 +1,29 @@
 require_relative 'node'
 
 describe Node do
+  let(:node) { Node.new(8) }
 
+  context '#new' do
+    it 'should have a value' do
+      expect(node.element).to eq(8)
+    end
+
+    it 'should point to nil' do
+      expect(node.next).to eq(nil)
+    end
+  end
+
+  context '#insert_after' do
+    it 'should insert a new node' do
+      next_node = Node.new(9)
+      expect(node.insert_after(next_node)).to eq(next_node)
+    end
+  end
+
+  context '#remove_after' do
+    it 'should remove the next node' do
+      node.remove_after
+      expect(node.next).to eq(nil)
+    end
+  end
 end
