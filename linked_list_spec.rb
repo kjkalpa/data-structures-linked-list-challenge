42fb1e02febb990d5137bbd91476650f25f3b37e
Kyriakos Kalpakidis <kjkalpa@gmail.com>
2017-02-10 12:36:41 -0600

    Create insert_first element method

--- linked_list_spec.rb
+++ linked_list_spec.rb
@@ -1,5 +1,32 @@
 require_relative 'linked_list'
 
+
 describe LinkedList do
+  let(:list) { LinkedList.new }
+  context '#new' do 
+    it 'Instantiate a new linked list' do 
+      new_list = LinkedList.new
+      expect(new_list.root_node).to eq(nil)
+    end
+  end
+
+  context '#insert_first' do 
+    it 'insert a new record at beginning' do 
+      list.insert_first(9) 
+      expect(list.root_node.element).to eq(9)
+    end
+    it 'insert a second record at beginning' do 
+      list.insert_first(9) 
+      list.insert_first(10) 
+      expect(list.root_node.element).to eq(10)
+    end
+    it 'next node should be available' do 
+      list.insert_first(9) 
+      list.insert_first(10) 
+      next_node = list.root_node.next
+      p next_node
+      expect(next_node.element).to eq(9)
+    end
+  end
 
 end
